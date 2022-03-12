ebf1fcfc835b4896652603035879f0a74c82592f
jin-sungdae <makonle@naver.com>
2022-03-13 02:46:39 +0900

    아오지 api 전부 생성 aoji index 수정

--- lms-api/src/main/java/com/larry/fc/finalproject/api/dto/aojidto/AojiDto.java
+++ lms-api/src/main/java/com/larry/fc/finalproject/api/dto/aojidto/AojiDto.java
@@ -9,7 +9,7 @@ import java.time.LocalDateTime;
 @Data
 @Builder
 public class AojiDto {
-    @Schema(description = "user aoji index", example = "1, 2,..")
+    @Schema(description = "user aoji index", example = "1")
     private final Long aojiTimeIndex;
     @Schema(description = "시작 시간", example = "2022-03-12 02:38:10.268987")
     private final LocalDateTime startAt;
