1392e7f95b433594adc9bd1348798f472e0839e8
jin-sungdae <makonle@naver.com>
2022-04-08 17:26:37 +0900

    port 80 443 4.

--- lms-api/src/main/java/com/larry/fc/finalproject/api/config/ConnectorConfig.java
+++ lms-api/src/main/java/com/larry/fc/finalproject/api/config/ConnectorConfig.java
@@ -15,20 +15,20 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class ConnectorConfig {
 
-
-        @Value("${server.port.http}")
-        private int serverPortHttp;
-
-        @Bean
-        public ServletWebServerFactory serverFactory() {
-            TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
-            tomcat.addAdditionalTomcatConnectors(createStandardConnector());
-            return tomcat;
-        }
-        private Connector createStandardConnector() {
-            Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
-            connector.setPort(serverPortHttp);
-            return connector;
-        }
+//
+//        @Value("${server.port.http}")
+//        private int serverPortHttp;
+//
+//        @Bean
+//        public ServletWebServerFactory serverFactory() {
+//            TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
+//            tomcat.addAdditionalTomcatConnectors(createStandardConnector());
+//            return tomcat;
+//        }
+//        private Connector createStandardConnector() {
+//            Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+//            connector.setPort(serverPortHttp);
+//            return connector;
+//        }
 }
 
