adfaba5fd2cb35ad47d4fc6f45886bdeba54b29f
jin-sungdae <makonle@naver.com>
2022-06-11 20:49:31 +0900

    Fix: cors localhost:3000 error 해결

--- lms-api/src/main/java/com/larry/fc/finalproject/api/config/WebConfig.java
+++ lms-api/src/main/java/com/larry/fc/finalproject/api/config/WebConfig.java
@@ -14,6 +14,6 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:8080", "http://localhost:8081", "http://localhost:3000");
+                .allowedOrigins("*");
     }
 }
