21b379b01d0b14c963ffe4b1f621fe0bd3805a70
Toysin <toysin1203@hotmail.com>
2022-07-07 16:42:50 +0900

    Rename : 통계 도메인 패키지를 생성

--- lms-core/src/main/java/com/savelms/core/statistical/DayStatisticalData.java
+++ lms-core/src/main/java/com/savelms/core/statistical/DayStatisticalData.java
@@ -1,20 +1,18 @@
-package com.savelms.core;
+package com.savelms.core.statistical;
 
+import com.savelms.core.BaseEntity;
+import com.savelms.core.attendance.domain.AttendanceStatus;
+import com.savelms.core.attendance.domain.entity.Attendance;
 import com.savelms.core.calendar.domain.entity.Calendar;
+import com.savelms.core.study_time.domain.entity.StudyTime;
+import com.savelms.core.todo.domain.entity.Todo;
 import com.savelms.core.user.domain.entity.User;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.Table;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+import lombok.*;
+
+import java.util.List;
 
 @AllArgsConstructor
 @NoArgsConstructor
@@ -70,6 +68,27 @@ public class DayStatisticalData extends BaseEntity {
     @JoinColumn(name="CALENDAR_ID")
     private Calendar calendar;
 
+    //연관관계를 todo, attendance, study_time 이랑 1대1로 묶는건 어떨까?
+
+//    @OneToOne(mappedBy = "statisticalData", fetch = FetchType.LAZY)
+//    private Attendance attendance;
+//
+//    @OneToMany
+//    private List<Todo> todos;
+//
+//    @OneToMany
+//    private List<StudyTime> studyTimes;
+
     /********************************* 비니지스 로직 *********************************/
 
+
+//    public void markAttendance(AttendanceStatus status) {
+//        //계속 반복해서 찍는거 제외해야함
+//        //아그냥 배치돌리고 오늘건 쿼리로 뽑아내는게 나을지도?
+//
+//        if (status == AttendanceStatus.PRESENT) {
+//            attendanceScore += 0.5;
+//        }
+//
+//    } -> 기각~~~~ 기존방식대로 하는게 더 좋을 듯
 }
