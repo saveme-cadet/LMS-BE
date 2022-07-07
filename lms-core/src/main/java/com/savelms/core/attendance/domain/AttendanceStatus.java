68a79914438cfb5abcd2b723dbe1a6c33c15fe14
Toysin <toysin1203@hotmail.com>
2022-07-07 16:38:02 +0900

    Refactor : CheckInStatus CheckOutStatus 를 하나로 통합

--- lms-core/src/main/java/com/savelms/core/attendance/domain/AttendanceStatus.java
+++ lms-core/src/main/java/com/savelms/core/attendance/domain/AttendanceStatus.java
@@ -1,5 +1,24 @@
 package com.savelms.core.attendance.domain;
 
+import lombok.Getter;
+
+@Getter
 public enum AttendanceStatus {
-    NONE, PRESENT, TARDY, ABSENT, OFFICIAL_ABSENT, ILLNESS, VACATION
+    NONE("없음", 0.0, 0.0),
+    PRESENT("출석", 0.5, 0.0),
+    TARDY("지각", 0.0, 0.25),
+    ABSENT("결석", 0.0, 0.5),
+    OFFICIAL_ABSENT("공결", 0.0, 0.0),
+    ILLNESS("병결", 0.0, 0.0),
+    VACATION("휴가", 0.0, 0.0);
+
+    private String value;
+    private Double attendanceScore;
+    private Double attendancePenalty;
+
+    AttendanceStatus(String value, Double attendanceScore, Double attendancePenalty) {
+        this.value = value;
+        this.attendanceScore = attendanceScore;
+        this.attendancePenalty = attendancePenalty;
+    }
 }
