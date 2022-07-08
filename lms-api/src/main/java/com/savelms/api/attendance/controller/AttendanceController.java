036c3aa3916387f728ed071f81890db992a0e009
Toysin <toysin1203@hotmail.com>
2022-07-09 00:37:41 +0900

    Refactor : 예외 발생 시 예외 메세지를 바디에 담아 반환하도록 변경

--- lms-api/src/main/java/com/savelms/api/attendance/controller/AttendanceController.java
+++ lms-api/src/main/java/com/savelms/api/attendance/controller/AttendanceController.java
@@ -7,6 +7,7 @@ import com.savelms.core.user.domain.repository.UserRepository;
 import lombok.Builder;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.User;
@@ -27,7 +28,7 @@ public class AttendanceController {
 
 
     @PatchMapping("/{attendanceId}/checkin")
-    public ResponseEntity<Void> userCheckIn(
+    public ResponseEntity<String> userCheckIn(
             @AuthenticationPrincipal User sessionUser,
             @PathVariable("attendanceId") Long attendanceId,
             @Valid @RequestBody CheckIOReq reqBody) {
@@ -43,19 +44,19 @@ public class AttendanceController {
             userRepository.findByUsername(sessionUser.getUsername())
                     .ifPresent(user -> attendanceService.checkIn(attendanceId, user, reqBody.getStatus()));
             //정상적으로 로직이 실행 된 경우
-            return ResponseEntity.ok().build();
+            return new ResponseEntity<>("OK", HttpStatus.OK);
         } catch (NoPermissionException e) {
             //변경 권한이 없는 유저가 변경을 시도한 경우
-            return ResponseEntity.badRequest().build();
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
         } catch (NoSuchElementException e) {
             //잘못된 출결표Id로 요청한 경우
-            return ResponseEntity.notFound().build();
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
         }
     }
 
 
     @PatchMapping("/{attendanceId}/checkout")
-    public ResponseEntity<Void> userCheckOut(
+    public ResponseEntity<String> userCheckOut(
             @AuthenticationPrincipal User sessionUser,
             @PathVariable("attendanceId") Long attendanceId,
             @Valid @RequestBody CheckIOReq reqBody) {
@@ -71,16 +72,16 @@ public class AttendanceController {
             userRepository.findByUsername(sessionUser.getUsername())
                     .ifPresent(user -> attendanceService.checkOut(attendanceId, user, reqBody.getStatus()));
             //정상적으로 로직이 실행 된 경우
-            return ResponseEntity.ok().build();
+            return new ResponseEntity<>("OK", HttpStatus.OK);
         } catch (NoPermissionException e) {
             //변경 권한이 없는 유저가 변경을 시도한 경우
-            return ResponseEntity.badRequest().build();
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
         } catch (IllegalStateException e) {
             //체크인을 하지 않고 체크아웃을 시도한 경우
-            return ResponseEntity.badRequest().build();
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
         } catch (NoSuchElementException e) {
             //잘못된 출결표Id로 요청한 경우
-            return ResponseEntity.notFound().build();
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
         }
     }
 
