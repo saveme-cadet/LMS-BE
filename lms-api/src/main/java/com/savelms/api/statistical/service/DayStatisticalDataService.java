24add72577119744221b7753d1855833fe03e691
Kim kyunghee <kkh2415@naver.com>
2022-07-16 00:16:20 +0900

    add : DayStatisticalDataService 추가

---
+++ lms-api/src/main/java/com/savelms/api/statistical/service/DayStatisticalDataService.java
@@ -0,0 +1,12 @@
+package com.savelms.api.statistical.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Transactional
+@Service
+public class DayStatisticalDataService {
+
+}
