492b1962995eba753949f8caa30bb29738c5700b
Kim kyunghee <kkh2415@naver.com>
2022-07-16 00:06:23 +0900

    add : findByUsernameAndDate 메서드 추가

--- lms-core/src/main/java/com/savelms/core/statistical/DayStatisticalDataRepository.java
+++ lms-core/src/main/java/com/savelms/core/statistical/DayStatisticalDataRepository.java
@@ -1,7 +1,15 @@
 package com.savelms.core.statistical;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.time.LocalDate;
+import java.util.Optional;
 
 public interface DayStatisticalDataRepository extends JpaRepository<DayStatisticalData, Long> {
     DayStatisticalData findByuser_idAndCalendar(Long id, Long calendar_id);
+
+    @Query("select d from DayStatisticalData d where d.user.username =:username and d.calendar.date =:date")
+    Optional<DayStatisticalData> findByUsernameAndDate(@Param("username") String username, LocalDate date);
 }
