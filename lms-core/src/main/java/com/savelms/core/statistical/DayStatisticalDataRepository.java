8a505307f6f115e977f5ee8fef0b33465c124eac
Kim kyunghee <kkh2415@naver.com>
2022-07-16 17:10:26 +0900

    add : #120 - DayStatisticalDataRepository에 findByApiIdAndDate 메서드 추가

--- lms-core/src/main/java/com/savelms/core/statistical/DayStatisticalDataRepository.java
+++ lms-core/src/main/java/com/savelms/core/statistical/DayStatisticalDataRepository.java
@@ -8,8 +8,13 @@ import java.time.LocalDate;
 import java.util.Optional;
 
 public interface DayStatisticalDataRepository extends JpaRepository<DayStatisticalData, Long> {
+
     DayStatisticalData findByuser_idAndCalendar(Long id, Long calendar_id);
 
     @Query("select d from DayStatisticalData d where d.user.username =:username and d.calendar.date =:date")
     Optional<DayStatisticalData> findByUsernameAndDate(@Param("username") String username, LocalDate date);
+
+    @Query("select d from DayStatisticalData d where d.user.apiId =:apiId and d.calendar.date =:date")
+    Optional<DayStatisticalData> findByApiIdAndDate(@Param("apiId") String apiId, LocalDate date);
+
 }
