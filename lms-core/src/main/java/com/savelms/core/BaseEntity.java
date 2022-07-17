03bc06001e005c372ca7f0da2960813c7dcc7c5c
jin-sungdae <makonle@naver.com>
2022-07-17 13:50:42 +0900

    Fix: baseEntity createAt updateAt 수정2

--- lms-core/src/main/java/com/savelms/core/BaseEntity.java
+++ lms-core/src/main/java/com/savelms/core/BaseEntity.java
@@ -11,6 +11,7 @@ import lombok.Setter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.springframework.format.annotation.DateTimeFormat;
 
 @Getter
 @Setter
@@ -22,12 +23,14 @@ public abstract class BaseEntity implements Serializable {
     @CreatedDate
     @Column(nullable = false, insertable = false, updatable = false,
             columnDefinition = "datetime default CURRENT_TIMESTAMP")
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
     private LocalDateTime createdAt;
     private String createId;
 
     @LastModifiedDate
     @Column(nullable = false, insertable = false, updatable = false,
             columnDefinition = "datetime default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP")
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
     private LocalDateTime updatedAt;
     private String UpdateId;
 
