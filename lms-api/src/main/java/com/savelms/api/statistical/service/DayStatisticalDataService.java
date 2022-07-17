37a7b62c8afff45641b35674dd8b8dfc1f31908e
Kim kyunghee <kkh2415@naver.com>
2022-07-17 12:44:48 +0900

    fix : #120 - statisticalDataService의  updateAttendanceAndAbsentScore메소드에 파라미터 추가

--- lms-api/src/main/java/com/savelms/api/statistical/service/DayStatisticalDataService.java
+++ lms-api/src/main/java/com/savelms/api/statistical/service/DayStatisticalDataService.java
@@ -1,12 +1,15 @@
 package com.savelms.api.statistical.service;
 
+import com.savelms.api.statistical.dto.DayStatisticalDataDto;
 import com.savelms.core.attendance.domain.AttendanceStatus;
+import com.savelms.core.statistical.DayStatisticalData;
 import com.savelms.core.statistical.DayStatisticalDataRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import javax.persistence.EntityNotFoundException;
 import java.time.LocalDate;
 
 @Slf4j
@@ -17,11 +20,31 @@ public class DayStatisticalDataService {
 
     private final DayStatisticalDataRepository statisticalDataRepository;
 
-    public void updateAttendanceAndAbsentScore(String username, AttendanceStatus status) {
-        statisticalDataRepository.findByUsernameAndDate(username, LocalDate.now())
+
+    /**
+     * 조회
+     * */
+    public DayStatisticalDataDto getDayStatisticalData(String username, LocalDate date) {
+        DayStatisticalData dayStatisticalData = statisticalDataRepository.findByUsernameAndDate(username, date)
+                .orElseThrow(() -> new EntityNotFoundException("존재하는 통계 테이블이 없습니다."));
+
+        return new DayStatisticalDataDto(dayStatisticalData);
+    }
+
+
+    /**
+     * 수정
+     * */
+    public void updateAttendanceAndAbsentScore(String username, AttendanceStatus status, LocalDate date) {
+        statisticalDataRepository.findByUsernameAndDate(username, date)
                 .ifPresentOrElse(dayStatisticalData -> {
                     dayStatisticalData.updateAttendanceScore(status.getAttendanceScore());
                     dayStatisticalData.updateAbsentScore(status.getAttendancePenalty());
+
+                    if (status.equals(AttendanceStatus.ABSENT)) {
+                        dayStatisticalData.updateWeekAbsentScore(1.0D);
+                    }
+
                 }, () -> {
                     log.warn("통계 테이블에 당일 레코드가 존재하지 않아 점수가 업데이트되지 않았습니다.");
                 });
@@ -36,13 +59,22 @@ public class DayStatisticalDataService {
                 });
     }
 
-    public void updateStudyTimeScore(String username, Double studyScore) {
-        statisticalDataRepository.findByUsernameAndDate(username, LocalDate.now())
+    public void updateStudyTimeScore(String username, Double studyScore, LocalDate date) {
+        statisticalDataRepository.findByUsernameAndDate(username, date)
                 .ifPresentOrElse(dayStatisticalData -> {
                     dayStatisticalData.updateStudyTimeScore(studyScore);
                 }, () -> {
                     log.warn("통계 테이블에 당일 레코드가 존재하지 않아 점수가 업데이트되지 않았습니다.");
                 });
+    }
+
+    public void updateTotalScore(String username, LocalDate date) {
+        DayStatisticalData dayStatisticalData = statisticalDataRepository.findByUsernameAndDate(username, date)
+                .orElseThrow(() -> new EntityNotFoundException("존재하는 통계 테이블이 없습니다."));
+
+        String month = String.format("%02d", date.getMonth().getValue());
+        statisticalDataRepository.findTotalStudyTimePerMonth(username, month)
+                .ifPresent(dayStatisticalData::updateTotalScore);
     }
 
 }
