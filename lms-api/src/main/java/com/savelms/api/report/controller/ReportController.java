5940897607b3936307736f2a0efdc5b1af7b83fe
jin-sungdae <makonle@naver.com>
2022-08-13 18:34:00 +0900

    Feat: Jenkins test6

--- lms-api/src/main/java/com/savelms/api/report/controller/ReportController.java
+++ lms-api/src/main/java/com/savelms/api/report/controller/ReportController.java
@@ -29,7 +29,7 @@ public class ReportController {
 
     private final ReportService reportService;
     private final UserRepository userRepository;
-    //test5
+    //test6
     /*
     위험한 유저 보고
      */
