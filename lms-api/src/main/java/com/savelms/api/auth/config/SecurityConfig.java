faa1ea412b477b2533c252ea5b2a5a6342e3821c
xogml123 <xogml951@gmail.com>
2022-08-13 16:34:13 +0900

    1

--- lms-api/src/main/java/com/savelms/api/auth/config/SecurityConfig.java
+++ lms-api/src/main/java/com/savelms/api/auth/config/SecurityConfig.java
@@ -47,8 +47,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                     .antMatchers(HttpMethod.POST, "/api/auth/logout").authenticated()
                     .antMatchers(HttpMethod.GET, "/api/auth/email*").permitAll()
                     .antMatchers("/api-docs/**").permitAll()
-           //         .antMatchers(HttpMethod.GET, "/api*").authenticated()
-
+                    .antMatchers("/**").authenticated()
                 //.mvcMatchers(HttpMethod.GET, "/").hasRole("USER")
                 //.mvcMatchers(HttpMethod.GET, "/api/userinfos").hasAnyRole("USER", "MEMBER")
             )
