d5c84d0655c82d82828a13a215213210245e8b05
Kim kyunghee <kkh2415@naver.com>
2022-08-29 21:22:44 +0900

    fix : 날짜별 유저 전체 정보 조회 API 리팩토링

--- lms-api/src/main/java/com/savelms/api/statistical/service/DayStatisticalDataService.java
+++ lms-api/src/main/java/com/savelms/api/statistical/service/DayStatisticalDataService.java
@@ -8,12 +8,10 @@ import com.savelms.api.user.userteam.service.UserTeamService;
 import com.savelms.api.vacation.service.VacationService;
 import com.savelms.core.attendance.domain.AttendanceStatus;
 import com.savelms.core.attendance.dto.AttendanceDto;
-import com.savelms.core.calendar.domain.repository.CalendarRepository;
 import com.savelms.core.statistical.DayStatisticalData;
 import com.savelms.core.statistical.DayStatisticalDataRepository;
 import com.savelms.core.team.TeamEnum;
 import com.savelms.core.user.role.RoleEnum;
-import com.savelms.core.user.role.domain.entity.Role;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -21,11 +19,9 @@ import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityNotFoundException;
 import java.time.LocalDate;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
-
-import static com.savelms.core.attendance.domain.AttendanceStatus.NONE;
+import java.util.stream.Collectors;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -33,52 +29,42 @@ import static com.savelms.core.attendance.domain.AttendanceStatus.NONE;
 @Service
 public class DayStatisticalDataService {
 
-    private final CalendarRepository calendarRepository;
-    private final AttendanceService attendanceService;
     private final VacationService vacationService;
     private final UserRoleService userRoleService;
     private final UserTeamService userTeamService;
+    private final AttendanceService attendanceService;
     private final DayStatisticalDataRepository statisticalDataRepository;
 
-//    public DayStatisticalDataDto getDayStatisticalData(String userId, LocalDate date) {
-//        Calendar calendar = calendarRepository.findByDate(date)
-//            .orElseThrow(() -> new EntityNotFoundException("해당 날짜의 캘린더가 존재하지 않습니다."));
-//
-//        DayStatisticalData dayStatisticalData = statisticalDataRepository.findByUserIdAndDate(userId, date)
-//                .orElseThrow(EntityNotFoundException::new);
-//
-//        return DayStatisticalDataDto.builder()
-//                .userId(dayStatisticalData.getUserId())
-//                .date(dayStatisticalData.getDate())
-//                .attendanceStatus(dayStatisticalData.getAttendanceStatus())
-//                .build();
-//    }
+    public List<DayLogDto> getDayLogs() {
+        final Map<Long, TeamEnum> teams = userTeamService.findAllUserTeamByDate(LocalDate.now());
+
+        return null;
+    }
 
     public List<DayLogDto> getDayLogs(LocalDate date) {
-        final List<DayLogDto> userLogDtoList = new ArrayList<>();
-        final Map<Long, AttendanceDto> attendances = attendanceService.getAllAttendanceByDate(date);
-        final Map<Long, Double> remainingVacations = vacationService.getAllRemainingVacationByDate(date);
-        final List<DayStatisticalData> dayStatisticalData = statisticalDataRepository.findAllByDate(date);
-        Map<Long, TeamEnum> teams = userTeamService.findAllUserTeamByDate(date);
-        Map<Long, RoleEnum> roles = userRoleService.findAllUserRoleByDate(date);
+        final Map<Long, TeamEnum> teams = userTeamService.findAllUserTeamByDate(date); //1
+        final Map<Long, RoleEnum> roles = userRoleService.findAllUserRoleByDate(date); // 1
+        final Map<Long, AttendanceDto> attendances = attendanceService.getAllAttendanceByDate(date); // 5
+        final Map<Long, Double> remainingVacations = vacationService.getAllRemainingVacationByDate(date); // 1
+        final List<DayStatisticalData> dayStatisticalData = statisticalDataRepository.findAllByDate(date); // 5
 
-        for (DayStatisticalData statisticalData : dayStatisticalData) {
-            Long userId = statisticalData.getUser().getId();
-            String apiId = statisticalData.getUser().getApiId();
-            String nickname = statisticalData.getUser().getNickname();
+        return dayStatisticalData.stream()
+                .map((statisticalData) -> {
+                    Long userId = statisticalData.getUser().getId();
+                    String apiId = statisticalData.getUser().getApiId();
+                    String nickname = statisticalData.getUser().getNickname();
 
-            Double vacation = remainingVacations.computeIfAbsent(userId, (k) -> 0.0);
-            TeamEnum team = teams.computeIfAbsent(userId, (k) -> TeamEnum.NONE);
-            RoleEnum role = roles.computeIfAbsent(userId, (k) -> RoleEnum.ROLE_UNAUTHORIZED);
-            AttendanceDto attendance = attendances.computeIfAbsent(userId,
-                    (k) -> new AttendanceDto(apiId, 0L, AttendanceStatus.NONE, AttendanceStatus.NONE));
+                    Double vacation = remainingVacations.computeIfAbsent(userId, (k) -> 0.0);
+                    TeamEnum team = teams.computeIfAbsent(userId, (k) -> TeamEnum.NONE);
+                    RoleEnum role = roles.computeIfAbsent(userId, (k) -> RoleEnum.ROLE_UNAUTHORIZED);
+                    AttendanceDto attendance = attendances.computeIfAbsent(userId,
+                            (k) -> new AttendanceDto(apiId, 0L, AttendanceStatus.NONE, AttendanceStatus.NONE));
 
-            DayLogDto userLogDto = DayLogDto.of(
-                    apiId, attendance.getAttendanceId(), nickname, attendance.getCheckInStatus(), attendance.getCheckOutStatus(),
-                    role, team, date, vacation, DayStatisticalDataDto.from(statisticalData));
-            userLogDtoList.add(userLogDto);
-        }
-        return userLogDtoList;
+                    return DayLogDto.of(apiId, attendance.getAttendanceId(), nickname,
+                            attendance.getCheckInStatus(), attendance.getCheckOutStatus(),
+                            role, team, date, vacation, DayStatisticalDataDto.from(statisticalData));
+                })
+                .collect(Collectors.toUnmodifiableList());
     }
 
     public DayStatisticalDataDto getDayStatisticalData(String username, LocalDate date) {
