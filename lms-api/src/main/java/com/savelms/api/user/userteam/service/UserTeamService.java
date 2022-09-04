e7543011b3db60986853f9a8568398adb2e965d6
Kim kyunghee <kkh2415@naver.com>
2022-09-04 16:20:54 +0900

    fix : day-logs API null 에러 처리

--- lms-api/src/main/java/com/savelms/api/user/userteam/service/UserTeamService.java
+++ lms-api/src/main/java/com/savelms/api/user/userteam/service/UserTeamService.java
@@ -3,12 +3,18 @@ package com.savelms.api.user.userteam.service;
 
 import com.savelms.core.team.TeamEnum;
 import com.savelms.core.team.domain.entity.UserTeam;
+import com.savelms.core.team.domain.repository.UserTeamRepository;
+import com.savelms.core.user.domain.entity.User;
 import com.savelms.core.user.domain.repository.UserRepository;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.Comparator;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
@@ -22,47 +28,17 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional(readOnly = true)
 public class UserTeamService {
 
+    private final UserTeamRepository userTeamRepository;
 
-    private final UserRepository userRepository;
-
-    //모든 유저의 특정날짜 team을 반환.
-    @Data
-    @AllArgsConstructor
-    private static class ForMapping {
-
-        private Long Id;
-        private TeamEnum teamEnum;
-    }
-    //모든 유저의 특정날짜 team을 반환.
     public Map<Long, TeamEnum> findAllUserTeamByDate(LocalDate date) {
+        Map<Long, TeamEnum> userTeams = new HashMap<>();
+        Map<User, List<UserTeam>> userAndUserTeams = userTeamRepository.findAllByDateBefore(LocalDateTime.of(date, LocalTime.MAX))
+                .stream().collect(Collectors.groupingBy(UserTeam::getUser));
 
-        return userRepository.findAllWithUserTeamsAndTeam()
-            .stream()
-            .map((u) -> (
-                new UserTeamService.ForMapping(u.getId(),
-                    findNearestUserTeam(u.getUserTeams(), date))
-            ))
-            .collect(Collectors.toMap(
-                UserTeamService.ForMapping::getId,
-                UserTeamService.ForMapping::getTeamEnum
-            ));
-    }
-
-    private TeamEnum findNearestUserTeam(List<UserTeam> userTeams, LocalDate date) {
-
-        TeamEnum result = null;
-        LocalDateTime findDay = date.plusDays(1L).atStartOfDay();
-        LocalDateTime resultTime = LocalDateTime.MIN;
-        for (UserTeam userTeam : userTeams) {
-            LocalDateTime createdAt = userTeam.getCreatedAt();
-            if (createdAt.isBefore(findDay)) {
-                if (createdAt.compareTo(resultTime) > 0) {
-                    result = userTeam.getTeam().getValue();
-                    resultTime = createdAt;
-                }
-            }
+        for (User user : userAndUserTeams.keySet()) {
+            UserTeam userTeam = userAndUserTeams.get(user).stream().max(Comparator.comparing(UserTeam::getCreatedAt)).get();
+            userTeams.put(user.getId(), userTeam.getTeam().getValue());
         }
-        return result;
+        return userTeams;
     }
-
 }
--- lms-core/src/main/java/com/savelms/core/team/domain/repository/UserTeamRepository.java
+++ lms-core/src/main/java/com/savelms/core/team/domain/repository/UserTeamRepository.java
@@ -1,7 +1,11 @@
 package com.savelms.core.team.domain.repository;
 
 import com.savelms.core.team.domain.entity.UserTeam;
+
+import java.time.LocalDateTime;
 import java.util.List;
+
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -9,6 +13,10 @@ import org.springframework.data.repository.query.Param;
 public interface UserTeamRepository extends JpaRepository<UserTeam, Long> {
 
     @Query("SELECT ut FROM UserTeam ut WHERE ut.user.id = :userId AND ut.currentlyUsed = true")
-    public List<UserTeam> currentlyUsedUserTeam(@Param("userId") Long userId);
+    List<UserTeam> currentlyUsedUserTeam(@Param("userId") Long userId);
+
+    @EntityGraph(attributePaths = "user")
+    @Query("SELECT ut FROM UserTeam ut WHERE ut.createdAt <= :date order by ut.createdAt desc")
+    List<UserTeam> findAllByDateBefore(@Param("date") LocalDateTime date);
 
 }
