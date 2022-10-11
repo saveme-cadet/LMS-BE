b4b5073bd0f6e918203cd7557ff4ece67ad74674
xogml123 <xogml951@gmail.com>
2022-10-11 21:59:05 +0900

    fix: emailauth handle logic

--- lms-api/src/main/java/com/savelms/api/auth/controller/EmailController.java
+++ lms-api/src/main/java/com/savelms/api/auth/controller/EmailController.java
@@ -12,6 +12,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -28,6 +29,11 @@ class EmailController {
 
     private final EmailService emailService;
     private final UserService userService;
+
+    @ExceptionHandler
+    public ResponseEntity<String> handleException(EmailAuthTokenNotFoundException e) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+    }
 
     @PostMapping("/auth/password-inquery")
     public ResponseEntity<Void> passwordInquery(@Validated @Parameter @RequestBody UserPasswordInqueryRequest request) {
