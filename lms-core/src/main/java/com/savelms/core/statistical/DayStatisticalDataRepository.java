2f672449b4b48e9532e387d04272dec41db67b62
sjin <wlstjdeo00@gmail.com>
2022-10-14 00:54:06 +0900

    Fix: statisticalDataRepository  수정3

--- lms-core/src/main/java/com/savelms/core/statistical/DayStatisticalDataRepository.java
+++ lms-core/src/main/java/com/savelms/core/statistical/DayStatisticalDataRepository.java
@@ -38,8 +38,8 @@ public interface DayStatisticalDataRepository extends JpaRepository<DayStatistic
     @Query("select d from DayStatisticalData d where d.user.username = :username and d.calendar.date = :date")
     Optional<DayStatisticalData> findByUsernameAndDate(@Param("username") String username, @Param("date") LocalDate date);
 
-//    @Query("select d from DayStatisticalData d where d.user.apiId = :apiId and d.calendar.date = :date")
-//    Optional<DayStatisticalData> findByApiIdAndDate(@Param("apiId") String apiId, @Param("date") LocalDate date);
+    @Query("select d from DayStatisticalData d where d.user.apiId = :apiId and d.calendar.date = :date")
+    Optional<DayStatisticalData> findByApiIdAndDate(@Param("apiId") String apiId, @Param("date") LocalDate date);
 
 
 
