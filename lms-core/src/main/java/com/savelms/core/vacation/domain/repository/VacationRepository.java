23e815481da33bcc5a3c1a646c38c1d27cc97432
Kim kyunghee <kkh2415@naver.com>
2022-10-17 21:42:00 +0900

    fix : VacationRepository에서 미사용 메서드 제거

--- lms-core/src/main/java/com/savelms/core/vacation/domain/repository/VacationRepository.java
+++ lms-core/src/main/java/com/savelms/core/vacation/domain/repository/VacationRepository.java
@@ -1,24 +1,18 @@
 package com.savelms.core.vacation.domain.repository;
 
-import com.savelms.core.user.AttendStatus;
 import com.savelms.core.vacation.domain.entity.Vacation;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.util.List;
-import java.util.Optional;
 
 public interface VacationRepository extends JpaRepository<Vacation, Long>, VacationQueryRepository {
 
     @Query(value = "select v.*, u.* from vacation v join user u on u.user_id = v.user_id " +
             "where date(v.created_at) <= :date order by v.vacation_id desc", nativeQuery = true)
     List<Vacation> findAllByDate(@Param("date") LocalDate date);
-
-    @Query("select v from Vacation v where v.user.username = :username and v.usedDays <> 0")
-    List<Vacation> findByUsernameAndUsedDaysNotZero(@Param("username") String username);
 
     @Query("select v from Vacation v where v.user.apiId = :apiId and v.usedDays <> 0")
     List<Vacation>findByApiIdAndUsedDaysNotZero(@Param("apiId") String apiId);
@@ -28,6 +22,4 @@ public interface VacationRepository extends JpaRepository<Vacation, Long>, Vacat
             "where date(v.created_at) <= :date and u.attend_status = :attendStatus " +
             "order by v.vacation_id desc", nativeQuery = true)
     List<Vacation> findAllByDateAttendStatus(@Param("date") LocalDate date, @Param("attendStatus") String attendStatus);
-
-    //List<Vacation> findAllBy
 }
