fbe28b17d9897d13623dc5bc237a809326ad38c1
xogml123 <xogml951@gmail.com>
2022-10-19 16:17:13 +0900

    fix: Attendance entity attendStatus추가 하고 데이터 추가

--- lms-core/src/main/java/com/savelms/core/attendance/domain/entity/Attendance.java
+++ lms-core/src/main/java/com/savelms/core/attendance/domain/entity/Attendance.java
@@ -4,6 +4,7 @@ import com.savelms.core.BaseEntity;
 import com.savelms.core.attendance.domain.AttendanceStatus;
 import com.savelms.core.calendar.domain.entity.Calendar;
 import com.savelms.core.statistical.DayStatisticalData;
+import com.savelms.core.user.AttendStatus;
 import com.savelms.core.user.domain.entity.User;
 import lombok.*;
 
@@ -36,6 +37,11 @@ public class Attendance extends BaseEntity {
     @Builder.Default
     private AttendanceStatus checkOutStatus = NONE;
 
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    @Builder.Default
+    private AttendStatus attendStatus = AttendStatus.PARTICIPATED;
+
     /********************************* FK 연관관계 매핑 *********************************/
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name="USER_ID", nullable = false, updatable = false)
@@ -51,6 +57,10 @@ public class Attendance extends BaseEntity {
         calendar.getAttendances().add(this);
     }
 
+    private void setAttendStatus(AttendStatus attendStatus) {
+        this.attendStatus = attendStatus;
+    }
+
     //==생성 메서드==//
     public static Attendance createAttendance(User user, Calendar calendar) {
         Attendance attendance = new Attendance();
@@ -58,6 +68,7 @@ public class Attendance extends BaseEntity {
         attendance.setCalendar(calendar);
         attendance.checkIn(NONE);
         attendance.checkOut(NONE);
+        attendance.setAttendStatus(AttendStatus.PARTICIPATED);
         return attendance;
     }
 
